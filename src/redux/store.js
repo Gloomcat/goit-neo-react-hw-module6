5759586a33d864e73e04932f929aafd5e425bbe6
Oleksii Aleksieiev <reneguarde@gmail.com>
2024-10-10 21:05:09 +0300

    Fix warnings related to functions serialization from redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,8 +4,19 @@ import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
+import { thunk } from 'redux-thunk';
+
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
+
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist/es/constants';
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
@@ -21,6 +32,13 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore redux-persist actions from serializable checks
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(thunk),
 });
 
 export const persistor = persistStore(store);
